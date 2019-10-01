ad3388ee91d11114321150749214e97d603064e8
romantix74 <romantix75@gmail.com>
2019-10-01 14:31:56 +0300

    working, but not clear

--- parser.py
+++ parser.py
@@ -10,9 +10,17 @@ with open('input.txt') as f:
   in_file = f.read().split(date_words)
 
  
-print(in_file[0])
-print(in_file[3])
+
   
-# for i in in_file:
-  # if date_words in i: 
-    # print(i)
+for i in in_file:
+  #print("===================")
+  for pokupka in i.split("Покупка"):
+    # не выводим лишние строки
+    if "Я\n" in pokupka:
+      continue
+    
+    pokupka = pokupka.split("\n")
+	#if ""
+    print(pokupka)
+
+  #print("-------------------\n")
