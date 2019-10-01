7248641b565703865d5ea3fc2616df206bfa3fb3
romantix74 <romantix75@gmail.com>
2019-10-01 16:33:31 +0300

    working, but not clear 2

--- parser.py
+++ parser.py
@@ -1,26 +1,51 @@
 #!/usr/bin/python
 
-
 # для выборки даты
 date_words = "СЕНТЯБР"
 
 in_file = []
 with open('input.txt') as f:
- # in_file = f.readlines()
-  in_file = f.read().split(date_words)
-
- 
-
+  in_file = f.readlines()
   
-for i in in_file:
+  #in_file = f.read().split(date_words)
+
+# для временного хренения данных покупки  
+temp = {}
+  
+for index,i in enumerate(in_file): #sorted(in_file, reverse=True):
   #print("===================")
-  for pokupka in i.split("Покупка"):
-    # не выводим лишние строки
-    if "Я\n" in pokupka:
-      continue
+   
+  # не выводим пустые строки
+  if i == "":
+    continue
+   
+     
+  if (date_words in i) or (index == (len(in_file)-1)):
     
-    pokupka = pokupka.split("\n")
-	#if ""
-    print(pokupka)
+    # if len(temp) > 0:
+      # print(temp.split("\n"))
+    _date = i
+    print(_date)
+    print("выводим покупки за число {}".format(_date))	
+    
+
+    continue
+  # обработка массива из покупок
+  
+  temp.update({str(index): i})
+  
+    
+  # for pokupka in i.split("Покупка"):
+    # # не выводим лишние строки
+    # if "Я\n" in pokupka:
+      # continue
+    
+    # pokupka = pokupka.split("\n")
+    # #if ""
+    # print(pokupka)
 
   #print("-------------------\n")
+for k,v in temp.items():
+  print(k)
+  print("===========")
+  print(v)
