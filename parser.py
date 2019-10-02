65cebf62e61b46db6a77671478e887b16b453dcb
romantix74 <romantix74@list.ru>
2019-10-02 14:28:28 +0300

    work , but format is incomaptible

--- parser.py
+++ parser.py
@@ -1,39 +1,66 @@
 #!/usr/bin/python
 
+"""
+формат такой примерно
+1 ОКТЯБРЯ
+Покупка OOO GAMMA-7, карта ********2746
+OOO GAMMA-7>Kugesi RU
+741,30 ₽
+Покупка PYATEROCHKA 4354, карта ********2746
+PYATEROCHKA 4354>KUGESI RU
+535,98 ₽
+"""
+
 # для выборки даты
-date_words = "СЕНТЯБР"
+date_words = "СЕНТЯБР" # "ОКТЯБР"
 
 in_file = []
-with open('input.txt') as f:
+with open('input_2_days.txt', encoding="utf-8") as f:
   in_file = f.readlines()
   
   #in_file = f.read().split(date_words)
 
-# для временного хренения данных покупки  
-temp = {}
+# вывод покупок раздельно в строку
+def print_pkp(_date,_temp_list):
+  for pkp in ''.join(_temp_list).split("Покупка"):
+      if pkp != '':
+        #print(_date + ';'  +''.join(pkp.split("\n")))
+        out = ''.join(pkp.split("\n"))  
+		# Mir Shkolnika, карта ********2746Mir Shkolnika>P KUGESI RU41 ?
+        print(out)
 
-# чтобы в словарь записывалть список покупок за день 
-pokupki = []
+# для временного хренения данных покупки  
+temp = []
+
+_date = ""
+
+
  
 for index,i in enumerate(in_file): #sorted(in_file, reverse=True):
-  #print("===================")
-   
-  # не выводим пустые строки
+     
+  # пропускаем пустые строки
   if i == "":
     continue
-   
-     
-  if (date_words in i) or (index == (len(in_file)-1)):
-    pokupki = []
-    # if len(temp) > 0:
-      # print(temp.split("\n"))
-    _date = i
-    temp.update({_date: pokupki})
-    print(_date)
-    print("выводим покупки за число {}".format(_date))	
+        
+  if (date_words in i):
+        
+    #update({_date: pokupki})
+    # for pkp in temp:
+      # print('{0} pokupka {1}'.format(_date, pkp))
+    #print("===================")
+    #print(temp)
+    print_pkp(_date, temp)
+    _date = i.split("\n")[0]
+    #print("-------------------\n")
+    temp = []
+    #print(_date)	
     continue
-  pokupki.append(i)    
-
+  if (index == (len(in_file)-1)):
+    temp.append(i)
+    print_pkp(_date, temp)
+  temp.append(i)
+      
+  
     
   # обработка массива из покупок
   
@@ -50,7 +77,7 @@ for index,i in enumerate(in_file): #sorted(in_file, reverse=True):
     # print(pokupka)
 
   #print("-------------------\n")
-for k,v in temp.items():
-  print(k)
-  print("===========")
-  print(v)
+# for k,v in temp.items():
+  # print(k)
+  # print("===========")
+  # print(v)
