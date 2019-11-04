7506fa425a6996013ee40222a15b0037e17aba87
romantix74 <romantix74@list.ru>
2019-11-04 10:02:14 +0300

    pts_payment is worked

--- parser_mts_payment_2.py
+++ parser_mts_payment_2.py
@@ -29,6 +29,9 @@ with open('input_mts_2.txt', encoding="utf-8") as f:
 # сумма в магазинах    
 shops = 0
 
+# заправки
+toplivo = 0
+
 # сумма не определена
 other = 0
 
@@ -38,28 +41,39 @@ all = 0
 # вывод покупок раздельно в строку
 def count_pkp(_date,_temp_list):
   global shops
+  global toplivo
   global other
   global all
   for pkp in ''.join(_temp_list).split("Покупка"):
+    
     if pkp != '':
       
       out = ''.join(pkp.split("\n"))  
 	  
 	  # ['   FRUKTY I OVOSHHI > Kugesi RU\t\n', '?131 руб.\n']
       rub = _temp_list[-1].replace(" руб.\n","").replace(" ","").replace(",",".")
-      rub = re.sub('[\?]', '', rub)
+      rub = re.sub('^.', '', rub)
       
-	  #rub = int(float(rub))
+      rub = int(float(rub))
       print(rub)		  
      
-      #all += rub
-      print(all)		  
+      all += rub
+      print(all)	  
+	  
+	  # определяем категорию товара
+      _category = _temp_list[1]
+      print(_category)
 	
-      # if ("magnit" in pkp) or ("gastronom" in pkp) or ("pyaterochka" in pkp) or ("zvenigovski" in pkp):
-        # shops += rub
-      # else:
-        # other += rub		
-      # print(out)
+      #if ("MAGNIT" in _category) or ("GASTRONOM" in _category) or ("PYATEROCHKA" in _category) or ("zvenigovski" in _category) or ("SPAR" in _category):
+      if any(re.findall(r'MAGNIT|novinka|lenta|PYATEROCHKA|zvenigovski|SPAR', _category, re.IGNORECASE)):
+        print("shops ADDED")
+        shops += rub
+      elif any(re.findall(r'TATNEFT', _category, re.IGNORECASE)):
+        print("toplivo ADDED")
+        toplivo +=rub
+      else:
+        other += rub		
+      print(out)
 
 # для временного хренения данных покупки  
 temp = []
@@ -80,13 +94,13 @@ for index,i in enumerate(in_file): #sorted(in_file, reverse=True):
     #update({_date: pokupki})
     # for pkp in temp:
       # print('{0} pokupka {1}'.format(_date, pkp))
-    #print("===================")
+    print("===================")
     print(temp)
     _date = i.split("\n")[0]
     #print(_date)
     count_pkp(_date, temp)
     
-    #print("-------------------\n")
+    print("-------------------\n")
     temp = []
     #print(_date)	
     continue
@@ -98,10 +112,11 @@ for index,i in enumerate(in_file): #sorted(in_file, reverse=True):
   
 ## ВЫВОД СУММ
 print("Магазины: {}".format(shops))
+print("Топливо: {}".format(toplivo))
 print("не определно: {}".format(other))
   
-print("Всего Магазины+Не_определено: {}".format(shops+other))
-print("Всего: {}".format(shops+other))
+print("Всего Магазины+Не_определено: {}".format(shops+toplivo+other))
+print("Всего: {}".format(shops+toplivo+other))
     
   # обработка массива из покупок
   
